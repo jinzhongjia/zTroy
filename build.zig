2a64b2f92c1611e83468420303b14a7ee7ab6ad6
jinzhongjia <mail@nvimer.org>
2024-06-26 10:39:34 +0800

    add basic file

---
+++ build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const troy = b.addModule("troy", .{
+        .root_source_file = b.path("src/zTroy.zig"),
+    });
+
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path("test/test.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    unit_tests.root_module.addImport("troy", troy);
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+}
---
+++ test/test.zig
@@ -0,0 +1,1 @@
+const troy = @import("troy");
