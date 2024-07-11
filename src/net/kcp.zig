03508c940533cd9fb86ec0b8d7e9d5a5c47b66c4
jinzhongjia <mail@nvimer.org>
2024-07-11 13:29:07 +0800

    add kcp file

---
+++ src/net/kcp.zig
@@ -0,0 +1,7 @@
+//! KCP is a fast and reliable protocol
+//! that can reduce the average delay by 30%-40%
+//! and reduce the maximum delay 
+//! by three times at the cost of wasting 10%-20% 
+//! of bandwidth than TCP.
+//!
+//! See this: https://github.com/skywind3000/kcp
